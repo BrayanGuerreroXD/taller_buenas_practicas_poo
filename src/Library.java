c39eb328c00e2db86a13fa234b3d278eb8a9ad99
BrayanGuerreroXD <bg7047653@gmail.com>
2022-06-30 20:25:46 -0500

    Se agregaron el metodo para crear un playlist en la clase Library

--- src/Library.java
+++ src/Library.java
@@ -11,6 +11,20 @@ public class Library implements PlayList {
         return songs;
     }
 
+    public ArrayList<Song> createPlayList(ArrayList<String> list) {
+        ArrayList<Song> playlist = new ArrayList<Song>();
+
+        for(String name: list) {
+            for(Song song: this.getSongs()) {
+                if(song.getTitle().equals(name)) {
+                    playlist.add(song);
+                }
+            }
+        }
+
+        return playlist;
+    }
+
     @Override
     public ArrayList<Song> filterByGender(String gender) {
         ArrayList<Song> filterList = new ArrayList<Song>();
@@ -28,8 +42,8 @@ public class Library implements PlayList {
     public ArrayList<Song> filterByYear(int year) {
         ArrayList<Song> filterList = new ArrayList<Song>();
 
-        for(Song song: this.getSongs()){
-            if(song.getDate().getYear() == year){
+        for(Song song: this.getSongs()) {
+            if(song.getDate().getYear() == year) {
                 filterList.add(song);
             }
         }
